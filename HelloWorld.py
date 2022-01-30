6f7c1d4e99b22a35c8a12aa5c9827c0bef062bc7
susmitha55dhulam <98656939+susmitha55dhulam@users.noreply.github.com>
2022-01-30 22:14:04 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,9 @@
-//This is comment
-print('Hello World');
+from flask import Flask
+app = Flask(__name__)
 
+@app.route("/")
+    def hello():
+        return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
